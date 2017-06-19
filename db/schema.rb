691192ef205d811dd6f01f0a00e705fcd92b7b74
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 17:23:11 +0000

    Integrated index action for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170619143356) do
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
-    t.text     "bodY"
+    t.text     "body"
     t.text     "main_image"
+    t.text     "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
